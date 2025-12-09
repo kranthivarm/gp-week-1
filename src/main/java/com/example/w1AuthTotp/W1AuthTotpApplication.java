a6219da110ce0c1cd9d6c6d1df3e024092c46d5d
kranthivarm <22a91a05g2@aec.edu.in>
2025-12-09 14:55:00 +0530

    Initialising project

---
+++ src/main/java/com/example/w1AuthTotp/W1AuthTotpApplication.java
@@ -0,0 +1,15 @@
+package com.example.w1AuthTotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1AuthTotpApplication {
+
+	public static void main(String[] args) {
+		System.out.println("every thing ok");
+		SpringApplication.run(W1AuthTotpApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/example/w1AuthTotp/controllers/cntrl.java
@@ -0,0 +1,11 @@
+package com.example.w1AuthTotp.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class cntrl {
+//    @PostMapping()
+}
